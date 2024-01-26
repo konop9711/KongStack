23067305b05e89001d897d65a9eba946049c6db3
konop9711 <157829096+konop9711@users.noreply.github.com>
2024-01-26 18:08:44 +0100

    Update contractAddresses.ts

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
-  "0x86eAf8D635eb11D5CD801c41744F44e50d4fd8d2";
+  "0x39572afF74b7A9ee6b300d7eD50347BFc0519895";
 export const tokenContractAddress =
-  "0x12ea05ba6E8405cBeceB5b51134A821e86858F7E";
+  "0xbFC83290D333931486b08B5105Ee108e11580C7F";
 export const stakingContractAddress =
-  "0x830Ace54DFbf3a011601bD0Bc1f9806d45D2d0E4";
+  "0x6364916d3b86efefF94a1e5B68B6997A034321aE";
